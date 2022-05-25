0a53bbdb418644bfd5c8219c9d517100bac131c9
Vadim Allayarov <105875799+VadimAlfredson@users.noreply.github.com>
2022-05-25 20:34:14 +0500

    add Friends

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Dialogs from './components/Dialogs/Dialogs.jsx';
 import Profile from './components/Profile/Profile.jsx';
+import Friends from './components/Friends/Friends.jsx';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App = (props) => {
@@ -12,12 +13,15 @@ const App = (props) => {
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
+                <Friends/>
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path='/dialogs/*' element={<Dialogs messages={props.messages} dialogs={props.dialogs}/>}/>
+                        <Route path='/dialogs/*'
+                               element={<Dialogs messages={props.messages} dialogs={props.dialogs}/>}/>
                         <Route path='/profile' element={<Profile posts={props.posts}/>}/>
                     </Routes>
                 </div>
+
             </div>
         </BrowserRouter>
     );
---
+++ src/components/Friends/Friends.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Friends.module.css';
+
+
+const Friends = () => {
+    return <div className={s.friends}>
+        <h4>Friends</h4>
+    </div>
+}
+
+export default Friends;
