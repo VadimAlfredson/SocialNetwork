c800ed86c5d0af0c6eda25664d9aaa548fd774e0
Vadim Allayarov <105875799+VadimAlfredson@users.noreply.github.com>
2022-05-29 22:49:53 +0500

    Add files via upload

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -10,7 +10,7 @@ const Dialogs = (props) => {
     );
 
     let messagesItem = props.messages.map(
-        m => <MessagesItem message={m.message} AddMessageInDialogs={props.AddMessageInDialogs}/>
+        m => <MessagesItem message={m.message} icon={m.icon} AddMessageInDialogs={props.AddMessageInDialogs}/>
     );
 
     return (
