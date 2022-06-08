6fa9626838e16933be0b6ab8a58825d960e214af
Vadim Allayarov <105875799+VadimAlfredson@users.noreply.github.com>
2022-06-08 23:31:57 +0500

    Add files via upload

---
+++ src/Redux/friends_reducer.js
@@ -0,0 +1,35 @@
+let initialState = {
+    friends: [
+        {
+            id: 1,
+            name: 'Alex',
+            photo: 'https://st3.depositphotos.com/1027110/18677/v/450/depositphotos_186777156-stock-illustration-dog-collection-swedish-vallhund-geometric.jpg'
+        },
+        {
+            id: 2,
+            name: 'Nikita',
+            photo: document.img = 'https://st3.depositphotos.com/1027110/18677/v/450/depositphotos_186777156-stock-illustration-dog-collection-swedish-vallhund-geometric.jpg'
+        },
+        {
+            id: 3,
+            name: 'Kamila',
+            photo: document.img = 'https://st3.depositphotos.com/1027110/18677/v/450/depositphotos_186777156-stock-illustration-dog-collection-swedish-vallhund-geometric.jpg'
+        },
+        {
+            id: 4,
+            name: 'Pavel',
+            photo: document.img = 'https://st3.depositphotos.com/1027110/18677/v/450/depositphotos_186777156-stock-illustration-dog-collection-swedish-vallhund-geometric.jpg'
+        },
+        {
+            id: 5,
+            name: 'Danil',
+            photo: document.img = 'https://st3.depositphotos.com/1027110/18677/v/450/depositphotos_186777156-stock-illustration-dog-collection-swedish-vallhund-geometric.jpg'
+        },
+    ]
+};
+
+let friendsReducer = (state = initialState, action) => {
+    return state
+}
+
+export default friendsReducer;
