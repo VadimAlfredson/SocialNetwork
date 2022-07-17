deee24d7db636d8ca705c99f4c2ccd958a8a6d7e
Vadim Allayarov <105875799+VadimAlfredson@users.noreply.github.com>
2022-07-17 21:29:15 +0500

    Add files via upload

--- src/Redux/reduxStore.ts
+++ src/Redux/reduxStore.ts
@@ -1,4 +1,4 @@
-import {combineReducers, legacy_createStore as createStore} from "redux";
+import {combineReducers, createStore} from "@reduxjs/toolkit";
 import profileReducer from "./profile_reducer";
 import dialogsReducer from "./dialogs_reducer";
 import friendsReducer from "./friends_reducer";
