4e1c137cabc0c43a4eb975a84094d2d4070461b1
Vadim Allayarov <105875799+VadimAlfredson@users.noreply.github.com>
2022-07-18 22:46:25 +0500

    componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,18 +5,28 @@ import s from "../Users/users.module.css"
 import * as axios from "axios";
 
 class Users extends React.Component {
-    getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(responce => {
-                    this.props.setUsers(responce.data.items)
-                })
-        }
-    }
+
+   componentDidMount(): void {
+       axios.get('https://social-network.samuraijs.com/api/1.0/users')
+           .then(responce => {
+               this.props.setUsers(responce.data.items)
+           })
+   }
+
+
+
+    /*    getUsers = () => {
+            if (this.props.users.length === 0) {
+                axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                    .then(responce => {
+                        this.props.setUsers(responce.data.items)
+                    })
+            }
+        }*/
     render(): React.ReactNode {
         return (
             <div>
-                <button onClick={this.getUsers}>Get Users</button>
+                {/*<button onClick={this.getUsers}>Get Users</button>*/}
                 {
                     this.props.users.map((u: UserType) => <div key={u.id}>
         <span>
@@ -24,7 +34,8 @@ class Users extends React.Component {
                 <img src={u.photos.small !== null ? u.photos.small :
                     'https://st3.depositphotos.com/1027110/18677/v/450/depositphotos_186777156-stock-illustration-dog-collection-swedish-vallhund-geometric.jpg'
                 } className={s.avatar}/>{
-                <button onClick={() => (this.props.followed(u.id))}>{u.followed === true ? 'Unfollew' : 'Follow'}</button>
+                <button
+                    onClick={() => (this.props.followed(u.id))}>{u.followed === true ? 'Unfollew' : 'Follow'}</button>
             }</div>
 <span>
     <div>{u.name}</div>
