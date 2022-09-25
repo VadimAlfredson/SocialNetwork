1446184cb45d04cb90189294e6c9e503ef0a527d
Vadim Allayarov <105875799+VadimAlfredson@users.noreply.github.com>
2022-09-25 21:35:41 +0500

    Formic Login

---
+++ src/components/login/formLogin.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import * as yup from "yup";
+
+
+export const LoginForm = () => {
+    const validationSchema = yup.object().shape({
+        email: yup.string().typeError('Incorrect email').required('required to fill out')
+        })
+    return <Formik
+        initialValues={{
+            email: '',
+            password: '',
+            checkbox: '',
+        }}
+        validateOnBlur
+        onSubmit={(values => { console.log(values)})}
+        validationSchema={validationSchema}
+    >
+        {({ values,
+              errors,
+              touched,
+              handleBlur,
+              handleChange,
+              isValid,
+              handleSubmit,
+              dirty}) => (
+            <div>
+                <input
+                type={'email'}
+                name={'email'}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                /><br/>
+                <input
+                    type={'password'}
+                    name={'password'}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                /><br/>
+                <input
+                    type={'checkbox'}
+                    name={'checkbox'}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.checkbox}
+                /><br/>
+                <button
+                disabled={!isValid && !dirty}
+                onClick={handleSubmit}
+                type={'submit'}
+                >login</button>
+            </div>
+        )}
+    </Formik>
+};
--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import {LoginForm} from "./formLogin";
 
 const Login = () => {
-    return <h1>Login!!!</h1>
+    return <div>
+    <h1>Login!!!</h1>
+    <LoginForm />
+    </div>
 };
 
 export default Login
