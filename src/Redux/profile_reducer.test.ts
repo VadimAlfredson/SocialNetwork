c2a84cf079e5668ef1f2a02d79e67569926662a3
Vadim Allayarov <105875799+VadimAlfredson@users.noreply.github.com>
2022-10-23 18:47:53 +0500

    test test profile addpost

---
+++ src/Redux/profile_reducer.test.ts
@@ -0,0 +1,18 @@
+import profile_reducer, {AddPostActionCreator, postsType} from "./profile_reducer";
+
+let state = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likeCount: 23},
+        {id: 2, message: 'Yo', likeCount: 12},
+        {id: 3, message: 'My first post!', likeCount: 32},
+    ] as Array<postsType>
+}
+
+it ('Test: new post should be added', () => {
+    let action = AddPostActionCreator('Test');
+    let newState = profile_reducer(state, action);
+    expect(newState.posts.length).toBe(4);
+    expect(newState.posts[0].message).toBe('Test')
+    expect(newState.posts[0].id).toBe(5)
+})
+
