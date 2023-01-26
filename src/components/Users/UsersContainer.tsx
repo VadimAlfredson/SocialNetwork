83883cb3aef8424d924fe2feed5fb220cf886c9a
VadimAlfredson <damonkvantel@gmail.com>
2023-01-26 22:53:55 +0500

    User Container hooks

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,7 +1,7 @@
 import React, {FC, useEffect} from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
-import {RootState, useAppSelector} from "../../Redux/reduxStore";
+import {RootState, useAppDispatch, useAppSelector} from "../../Redux/reduxStore";
 import {
     followed,
     setCurrentPage,
@@ -16,31 +16,25 @@ import {
 import {compose} from "redux";
 import {putDialogUserThunkCreator} from "../../Redux/dialogs_reducer";
 
-type PropsType = {
-    getUsersThunkCreator: (currentPage: number, pageSize: number) => void
-    onChangeUsersThunkCreator: (pageNumber: number, pageSize: number, term: string, friend?: boolean) => void
-    onFollowChangeThunkCreator: (userId: number, follow: boolean) => void
-    putDialogUserThunkCreator: (userId: number) => {}
-}
 
-
-const UsersContainer: FC<PropsType> = (props) => {
+const UsersContainer: FC<{}> = (props) => {
+    const dispatch = useAppDispatch()
     const pageSize =  useAppSelector(state => state.users.pageSize)
     const currentPage = useAppSelector(state => state.users.currentPage)
     useEffect(() => {
-        props.getUsersThunkCreator(currentPage, pageSize)}, []
+        dispatch(getUsersThunkCreator(currentPage, pageSize))}, []
     )
 
     let onUsersChange = (pageNumber: number, pageSize: number, term: string, friend?: boolean) => {
-        props.onChangeUsersThunkCreator(pageNumber, pageSize, term, friend)
+        dispatch(onChangeUsersThunkCreator(pageNumber, pageSize, term, friend))
     }
 
     let onFollowChange = (userId: number, follow: boolean) => {
-        props.onFollowChangeThunkCreator(userId, follow)
+        dispatch(onFollowChangeThunkCreator(userId, follow))
     }
 
     let onDialogUserChange = (userId: number) => {
-        props.putDialogUserThunkCreator(userId)
+        dispatch(putDialogUserThunkCreator(userId))
     }
 
         return <>
@@ -54,17 +48,4 @@ const UsersContainer: FC<PropsType> = (props) => {
 
 }
 
-let mapStateToProps = (state: RootState) => {
-    return {
-    }
-}
-
-export default compose(
-    connect(mapStateToProps,
-        {
-            getUsersThunkCreator,
-            onChangeUsersThunkCreator,
-            onFollowChangeThunkCreator,
-            putDialogUserThunkCreator,
-        })
-)(UsersContainer)
+export default UsersContainer
