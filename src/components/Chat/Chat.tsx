c5c0a6f1c9b27510713e419b41facf43dc3e82f6
VadimAlfredson <damonkvantel@gmail.com>
2023-02-09 00:40:01 +0500

    chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, {FC} from "react";
+
+
+type MessagesChatType = {
+    name: string
+    message: string
+    icon: string
+    id: number
+}
+
+let messagesChat: MessagesChatType[] = [{name: 'test', message: 'Hi!', icon: '', id: 1}]
 
 export const Chat = () => {
-    
+    return <div></div>
+}
+
+const MessagesChat = () => {
+    return messagesChat.map(m =>
+        <MessageChat
+            key={m.id}
+            icon={m.icon}
+            message={m.message}
+            name={m.name}
+        />
+    )
+}
+
+const MessageChat: FC<MessagesChatType> = (props) => {
+    return <div>
+        <img src={props.icon}/>
+        <div>{props.name}</div>
+        <div>{props.message}</div>
+    </div>
 }
