aa78807a7c96d40717c39419a9a9598480096857
VadimAlfredson <damonkvantel@gmail.com>
2023-02-15 00:06:24 +0500

    navlink chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -3,6 +3,7 @@ import s from './Chat.module.css'
 import {withAuthNavigate} from "../hoc/witAuthNavigate";
 import {useAppDispatch, useAppSelector} from "../../Redux/reduxStore";
 import {setMessagesChatActionCreator} from "../../Redux/chat_reducer";
+import {NavLink} from "react-router-dom";
 
 
 type MessagesChatType = {
@@ -39,6 +40,7 @@ const MessagesChat: React.FC = (props) => {
     return <div  className={s.messagesChat}>{messagesChat.map((m: MessagesChatType, index) =>
         <MessageChat
             key = {index}
+            userId={m.userId}
             icon={m.photo}
             message={m.message}
             name={m.userName}
@@ -51,11 +53,12 @@ type PropsType = {
     icon: string
     name: string
     message: string
+    userId: number
 }
 const MessageChat: FC<PropsType> = (props) => {
     return <div className={s.messageBlock}>
-        <img className={s.avatar} src={props.icon ? props.icon : 'https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg'}/>
-        <div className={s.name}>{props.name}</div>
+        <NavLink className={s.avatar} to={`/profile/${props.userId}`} ><img className={s.avatar} src={props.icon ? props.icon : 'https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg'}/> </NavLink>
+        <NavLink className={s.name} to={`/profile/${props.userId}`} > <div className={s.name}>{props.name}</div> </NavLink>
         <div className={s.message}>{props.message}</div>
     </div>
 }
