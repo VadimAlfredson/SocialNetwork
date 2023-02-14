f3f095aec10cf292f58d9ea5bc75bf597be60c4f
VadimAlfredson <damonkvantel@gmail.com>
2023-02-14 23:48:22 +0500

    like in profile

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Post.module.css';
 
-const Post = (props: {message: string, likeCount: number}) => {
+const Post = (props: { message: string, likeCount: number }) => {
+    let [likeActive, setLikeActive] = useState<boolean>(false)
+    let [likeCount, setLikeCount] = useState<number>(props.likeCount)
+    let onClickChange = () => {
+        setLikeActive(!likeActive)
+        setLikeCount(!likeActive ? likeCount + 1 : likeCount - 1)
+    }
+
     return (
-        <div className={s.post} >
-            <img className={s.img} src='https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg'/>
-            <div className={s.messageBlock}><div className={s.message}>{props.message}</div></div>
-            <div className={s.like}>{props.likeCount} like</div>
+        <div className={s.post}>
+            <img className={s.img}
+                 src='https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg'/>
+            <div className={s.messageBlock}>
+                <div className={s.message}>{props.message}</div>
+            </div>
+            <div className={likeActive ? s.likeActive : s.like}>{likeCount}<img
+                className={s.likeIcon}
+                src={likeActive ? process.env.PUBLIC_URL + '/likeActive.png' : process.env.PUBLIC_URL + '/like.png'}
+                onClick={() => onClickChange()}
+                alt={'like'}/>
+            </div>
         </div>
     )
 }
